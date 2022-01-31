537246aa4ead2f0a1356b6b10d4802c722404407
Reynold Osei Adade <reynold@walulel.com>
2022-01-31 09:39:11 +0000

    setup routes

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+//import router
+import { createRouter, createWebHistory } from "vue-router";
+import EventList from "@/views/EventList.vue";
+import EventDetails from "@/views/EventDetails.vue";
+import EventCreate from "@/views/EventCreate.vue";
+import ErrorDisplay from "@/views/ErrorDisplay.vue";
+import About from "@/views/About.vue";
+
+const routes = [
+  { path: "/", component: EventList, name: "EventList" },
+  { path: "/event/:id", component: EventDetails, name: "EventDetails" },
+  { path: "/event/create", component: EventCreate, name: "EventCreate" },
+  { path: "/about", component: About, name: "About" },
+  { path: "/error/:/error", component: ErrorDisplay, name: "ErrorDisplay" },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
